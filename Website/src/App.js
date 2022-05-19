cfa0f027f7782a0acdb1d97a28c16b868ec84dd8
odivalq <odival.quaresma@gmail.com>
2022-05-19 10:49:48 -0300

    parte de login do website adicionado

---
+++ Website/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React, { Component }  from 'react';
+
+import AppRoutes from "./AppRoutes";
+
+function App() {
+  return (
+    <div className="app">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
